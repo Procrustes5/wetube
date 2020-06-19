8f7059ff2aea19834c261bd9bd70b905179138e5
Procrustes5 <procrustes5@gmail.com>
2020-06-20 01:44:53 +0900

    0620 Controllers Setting

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,12 +1,16 @@
 import { videos } from "../db"
+import routes from "../routes";
 
 export const home = (req, res) => {
 
     res.render("home", { pageTitle: "Home", videos });
 };
 export const search = (req, res) => {
-    const {query: { term: searchingBy }} = req;
-    res.render("search", { pageTitle: "Search", searchingBy });
+
+    const {
+        query: { term: searchingBy }
+    } = req;
+    res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 export const videoDetail = (req, res) => 
@@ -18,5 +22,13 @@ res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) => 
 res.render("deleteVideo", { pageTitle: "Delete Video" });
 
-export const upload = (req, res) => 
-res.render("upload", { pageTitle: "Upload" });
+export const getUpload = (req, res) => {
+    res.render("upload", { pageTitle: "Upload" });
+};
+export const postUpload = (req, res) => {
+    const {
+        body: { file, title, description} 
+    } = req;
+    // To do: Upload and save video
+    res.redirect(routes.videoDetail(324393));
+};
