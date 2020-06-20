52f781c008b907b1f30381eb9b54681ca5f4b10e
Procrustes5 <procrustes5@gmail.com>
2020-06-20 18:35:27 +0900

    0620 Starting Mongo DB

--- init.js
+++ init.js
@@ -1,7 +1,11 @@
 import app from "./app";
+import "./db";
+import dotenv from "dotenv";
+dotenv.config();
+import Video from "./models/Video";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+const handleListening = () => console.log(`♬ Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
