23fa52aaa47f5bf053966474d2a45e262c88f056
Procrustes5 <procrustes5@gmail.com>
2020-06-29 14:59:23 +0900

    0629 Video/Comment Models

--- init.js
+++ init.js
@@ -2,7 +2,8 @@ import app from "./app";
 import "./db";
 import dotenv from "dotenv";
 dotenv.config();
-import Video from "./models/Video";
+import Video from "./models/Video"; 
+import Comment from "./models/Comment"; 
 
 const PORT = process.env.PORT || 4000;
 
